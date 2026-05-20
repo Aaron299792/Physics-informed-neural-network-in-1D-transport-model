aacf45fe7f276aedc75aa7dbae2697aa69906c76
Aaron Sanabria <aaronsm01@proton.me>
2026-05-20 17:35:53 -0600

    feat(parameter managment) the code was restructure to take arguments and give written outputs.

--- src/drivers/pinn.py
+++ src/drivers/pinn.py
@@ -2,6 +2,8 @@
 # Modules
 # -------------------------------
 import numpy as np
+from ruamel.yaml import YAML
+import argparse
 import torch
 import torch.nn.functional as F
 import deepxde as dde
@@ -17,29 +19,64 @@ sys.path.append(os.path.abspath('../modules'))
 
 from sigmav2D import RateCoeff2D
 
+parser = argparse.ArgumentParser(description='Parameter file name')
+
+parser.add_argument('--f', action='store', dest='param_file', type=str,
+                    help='[STR] filename of the parameter file')
+parser.add_argument('--o', action='store', dest='out_file', type=str,
+                    help='[STR] filename of the output file')
+
+
+args = parser.parse_args()
+param_file = args.param_file
+out_file = args.out_file
+assert param_file != None, 'Input File was not find. Make sure it exists in the path "../../parameters". Usage --f [input filename] --o [output filename]'
+assert out_file != None, 'Output file not given. Usage --f [input filename] --o [output filename]'
+
+yaml = YAML(typ='safe')
+pathInput = '../../parameters/' + param_file
+pathOutput = '../../data/profile/' + out_file
+
+with open(pathInput, 'r') as file:
+    params = yaml.load(file)
 # -------------------------------
 # Global Constants and Parameters
 # -------------------------------
-B = 1 #T
-E_ION = 13.6 #eV
-T_MAX = 7000 # eV
-N_MAX = 2e13 # cm^-3
-R = 72 # cm
+B = params['physical_params']['B'] #T
+E_ION = params['physical_params']['E_ION'] #eV
+N_0 = params['physical_params']['N_0'] # cm^{-3}
+N_MIN = params['physical_params']['N_MIN']
+T_MIN = params['physical_params']['T_MIN']
+N_MAX = params['physical_params']['N_MAX'] # eV
+T_MAX = params['physical_params']['T_MAX'] # cm^-3
+R = params['physical_params']['R'] # cm
+LAMBDA_N = params['physical_params']['LAMBDA_N']
+LAMBDA_T = params['physical_params']['LAMBDA_T']
+P_EXT = params['physical_params']['P_EXT'] # cm^{-3}
+VAR = params['physical_params']['VAR']
+MU = params['physical_params']['MU']
+
 D_0 = 1e4 * (2 * np.sqrt(2 * np.pi * electron_mass) / 3) * (e / (4 * np.pi * epsilon_0 * B))**2 * (N_MAX * 1e6  / np.sqrt(T_MAX * e)) #cm^2 s^-1
 KAPPA_0 = 4.7 * N_MAX * D_0 # cm^-1 s^-1
 C = 2 * np.log(4 * np.pi * np.power(epsilon_0 * T_MAX * e, 1.5) / (e**3 * np.sqrt(N_MAX)))
-LAMBDA_N = 1.0
-LAMBDA_T = 1.0
-P_EXT = 1e18 #eV cm^-3 s^-1
-VAR = 0.15
-MU = 0.1
-N_0 = 1e8 # cm^{-3}
+
+#--------------------------------
+# MODEL PARAMETERS
+#--------------------------------
+HIDDEN_LAYERS = params['hidden_layers']
+NUM_DOMAIN = params['model_params']['NUM_DOMAIN']
+NUM_BOUNDARY = params['model_params']['NUM_BOUNDARY']
+NUM_TEST = params['model_params']['NUM_TEST']
+LOSS_WEIGHTS = params['model_params']['LOSS_WEIGHTS']
+ADAM_ITER1 = params['model_params']['ADAM_ITER1']
+ADAM_ITER2 = params['model_params']['ADAM_ITER2']
+OBSERVATIONS = params['num_observations']
 
 # -------------------------------
 # Device Settings
 # -------------------------------
 DTYPE = torch.float64
-EPS   = 1e-40
+EPS   = params['epsilon']
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 torch.set_default_device(DEVICE)
 
@@ -57,11 +94,11 @@ power loss rates:
     p_rad_0: <E_rad^0><sigma nu>_rad^0 n
 ------------------------------"""
 
-sigma_ion = RateCoeff2D('../../data/scd96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
-sigma_rec = RateCoeff2D('../../data/acd96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
-p_rec = RateCoeff2D('../../data/prb96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
-p_rad_i = RateCoeff2D('../../data/plt96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
-p_rad_0 = RateCoeff2D('../../data/prc96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
+sigma_ion = RateCoeff2D('../../data/adas/scd96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
+sigma_rec = RateCoeff2D('../../data/adas/acd96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
+p_rec = RateCoeff2D('../../data/adas/prb96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
+p_rad_i = RateCoeff2D('../../data/adas/plt96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
+p_rad_0 = RateCoeff2D('../../data/adas/prc96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
 
 
 # -------------------------------
@@ -73,7 +110,7 @@ def ode_system(X, y):
     # Preamble
     #----------------------------
     rho = X[:, 0:1]
-    rho_eps = torch.clamp(rho, min=1e-6) #no puede ser cero, las ecs explotan
+    rho_eps = torch.clamp(rho, min=1e-12) #no puede ser cero, las ecs explotan
     n_hat = y[:, 0:1]
     T_hat = y[:, 1:2]
 
@@ -84,23 +121,22 @@ def ode_system(X, y):
     dT_rho = dde.grad.jacobian(y, X, i=1, j=0)
 
     # T^3/n and log(T^3/n)
-    T3n = torch.clamp((T_hat ** 3) / (n_hat + EPS), min=1e-12,max=1e12)
+    T3n = (T_hat ** 3) / (n_hat)
     logT3n = torch.log(T3n)
-    D_hat = n_hat / torch.sqrt(T_hat + EPS) * (logT3n + C)
-    D_hat = torch.clamp(D_hat, min=1e-8, max=1e8)
+    D_hat = n_hat / torch.sqrt(T_hat) * (logT3n + C)
 
     #----------------------------
     # ODE: Particles
     #----------------------------
-    ionization = torch.nan_to_num(sigma_ion.rate(n, T), nan=0.0)
-    recombination = torch.nan_to_num(sigma_rec.rate(n, T), nan=0.0)
+    ionization = sigma_ion.rate(n, T) * n_hat**2
+    recombination = sigma_rec.rate(n, T) * n_hat * N_0
 
 
-    S_particle = (R**2 / (D_0 * N_MAX)) * (ionization - recombination)
+    S_particle = (R**2 / (D_0 * N_MAX)) * (- ionization + recombination)
     flux_n = rho_eps * D_hat * dn_rho
     dflux_n_rho = dde.grad.jacobian(flux_n, X, i=0, j=0)
 
-    ode1 = dflux_n_rho / rho_eps  + S_particle
+    ode1 = dflux_n_rho / rho_eps  - S_particle
 
     #----------------------------
     # ODE: Energy
@@ -113,9 +149,9 @@ def ode_system(X, y):
     gaussian_profile = torch.exp(-0.5 * ((rho - MU) / VAR)**2)
     power_dep = (P_EXT * R / (N_MAX * T_MAX * D_0)) * gaussian_profile
 
-    P_rec = torch.nan_to_num(p_rec.rate(n, T), nan=0.0)
-    P_rad_i = torch.nan_to_num(p_rad_i.rate(n,T), nan=0.0)
-    P_rad_0 = torch.nan_to_num(p_rad_0.rate(n,T), nan=0.0)
+    P_rec = p_rec.rate(n, T)
+    P_rad_i = p_rad_i.rate(n,T)
+    P_rad_0 = p_rad_0.rate(n,T)
 
     P_loss = P_rec + P_rad_i + P_rad_0
 
@@ -123,8 +159,8 @@ def ode_system(X, y):
 
     ode2 = conduction + power_dep - loss_power #me falta agregar coulomb
 
-    ode1 = torch.nan_to_num(ode1, nan=0.0)
-    ode2 = torch.nan_to_num(ode2, nan=0.0)
+    #ode1 = torch.nan_to_num(ode1, nan=0.0)
+    #ode2 = torch.nan_to_num(ode2, nan=0.0)
 
     return [ode1, ode2]
 
@@ -172,10 +208,9 @@ bc_r_T = dde.icbc.RobinBC(geom, robin_T, boundary_r, component=1)
 
 boundaries = [bc_l_n, bc_l_T, bc_r_n, bc_r_T]
 
-data = dde.data.PDE(geom, ode_system, boundaries, 300, 2, num_test=300)
+data = dde.data.PDE(geom, ode_system, boundaries, NUM_DOMAIN, NUM_BOUNDARY, num_test=NUM_TEST)
 
-
-net = dde.nn.FNN([1] + [64]*6 + [2], "tanh", "Glorot normal")
+net = dde.nn.FNN([1] + HIDDEN_LAYERS*6 + [2], "tanh", "Glorot normal")
 
 def output_transform(x, y):
     return torch.exp(y)
@@ -186,35 +221,23 @@ net.apply_output_transform(lambda x, y: torch.exp(y))
 
 model = dde.Model(data, net)
 
-loss_weights = [100, 100, 1, 1, 100, 100]
+model.compile("adam", lr=1e-4, loss_weights=LOSS_WEIGHTS)
+model.train(iterations=ADAM_ITER1)
 
-model.compile("adam", lr=1e-3, loss_weights=loss_weights)
-model.train(iterations=5000)
-
-model.compile("adam", lr=1e-4, loss_weights=loss_weights)
-model.train(iterations=10000)
+model.compile("adam", lr=1e-5, loss_weights=LOSS_WEIGHTS)
+model.train(iterations=ADAM_ITER2)
 
 model.compile("L-BFGS")
 model.train()
 
-x = np.linspace(0, 1, 500)[:, None]
+x = np.linspace(0, 1, OBSERVATIONS)[:, None]
 y = model.predict(x)
 
 n_pred = y[:, 0]
 T_pred = y[:, 1]
 
-plt.figure()
-plt.plot(x, n_pred)
-plt.xlabel("x")
-plt.ylabel("n(x)")
-plt.title("Density")
-plt.grid()
-plt.savefig('../../plots/density_profile.pdf', format='pdf', bbox_inches='tight')
+with open(pathOutput, 'w') as f:
+    for i in range(len(n_pred)):
+        print(x[i][0], " ", n_pred[i], " ", T_pred[i], file=f, sep='')
 
-plt.figure()
-plt.plot(x, T_pred)
-plt.xlabel("x")
-plt.ylabel("T(x)")
-plt.title("Temperature")
-plt.grid()
-plt.savefig('../../plots/temperature_profile.pdf', format='pdf', bbox_inches='tight')
+print("Output written in '../../data/profile'")
