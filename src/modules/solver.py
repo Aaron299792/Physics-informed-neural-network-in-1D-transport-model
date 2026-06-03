d4c9b2b7c27f2f7bcb6cc44aa80653d5ffc82068
Aaron Sanabria <aaronsm01@proton.me>
2026-06-03 17:06:48 -0600

    (feat) PINN solver module added

---
+++ src/modules/solver.py
@@ -0,0 +1,49 @@
+import numpy as np
+import deepxde as dde
+import torch
+from torch.nn.functional import softplus
+
+class pinn:
+    def __init__(self, input_size,
+                       hidden_size,
+                       output_size,
+                       domain_size,
+                       boundary_size,
+                       test_size,
+                       iter_pre,
+                       iter_size,
+                       solution = None
+                       ):
+
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.domain_size = domain_size
+        self.boundary_size = boundary_size
+        self.test_size = test_size
+        self.iter_pre = iter_pre
+        self.iter_size = iter_size
+        self.solution = solution
+
+    def sol(self, ode_func, geom, boundaries, x_eval, weights=None, refinement=False):
+        num_layers = int(len(boundaries) + self.output_size)
+        data = dde.data.PDE(geom, ode_func, boundaries, self.domain_size,
+                            self.boundary_size, num_test = self.test_size, solution=self.solution)
+
+        net = dde.nn.FNN([self.input_size] + [self.hidden_size] * num_layers + [self.output_size],
+                          "tanh", "Glorot normal")
+
+        net.apply_output_transform(lambda x, y: torch.exp(y) / (1 + torch.exp(y)))
+
+        model = dde.Model(data, net)
+
+        model.compile("adam", lr=1e-3, loss_weights=weights)
+        model.train(iterations=self.iter_pre)
+
+        model.compile("adam", lr=1e-4, loss_weights=weights)
+        model.train(iterations=self.iter_size)
+
+        if (refinement == True):
+            model.compile("L-BFGS")
+            model.train()
+        return model.predict(x_eval), model.predict(x_eval, operator = ode_func)
