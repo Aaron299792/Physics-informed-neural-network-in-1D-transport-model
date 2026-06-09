0167e5df23b584a22e6a175b3d83d84b38b3d3f2
Aaron Sanabria <aaronsm01@proton.me>
2026-06-09 16:04:22 -0600

    renaming and code updating

--- src/drivers/pinn.py
+++ src/drivers/pinn.py
@@ -15,9 +15,10 @@ from cherab.openadas.parse.adf11 import parse_adf11
 import sys
 import os
 
-sys.path.append(os.path.abspath('../modules'))
+sys.path.append(os.path.abspath('../'))
 
-from sigmav2D import RateCoeff2D
+from modules import pinn
+from modules import RateCoeff2D as sv
 
 parser = argparse.ArgumentParser(description='Parameter file name')
 
@@ -44,22 +45,21 @@ with open(pathInput, 'r') as file:
 # -------------------------------
 B = params['physical_params']['B'] #T
 E_ION = params['physical_params']['E_ION'] #eV
-N_0 = params['physical_params']['N_0'] # cm^{-3}
+N0 = params['physical_params']['N_0'] # cm^{-3}
 N_MIN = params['physical_params']['N_MIN']
 T_MIN = params['physical_params']['T_MIN']
 N_MAX = params['physical_params']['N_MAX'] # eV
 T_MAX = params['physical_params']['T_MAX'] # cm^-3
 R = params['physical_params']['R'] # cm
-LAMBDA_N = params['physical_params']['LAMBDA_N']
-LAMBDA_T = params['physical_params']['LAMBDA_T']
 P_EXT = params['physical_params']['P_EXT'] # cm^{-3}
 VAR = params['physical_params']['VAR']
 MU = params['physical_params']['MU']
-
-D_0 = 1e4 * (2 * np.sqrt(2 * np.pi * electron_mass) / 3) * (e / (4 * np.pi * epsilon_0 * B))**2 * (N_MAX * 1e6  / np.sqrt(T_MAX * e)) #cm^2 s^-1
-KAPPA_0 = 4.7 * N_MAX * D_0 # cm^-1 s^-1
-C = 2 * np.log(4 * np.pi * np.power(epsilon_0 * T_MAX * e, 1.5) / (e**3 * np.sqrt(N_MAX)))
-
+l = params['physical_params']['l']
+h = (e / (epsilon_0 * B))**2 * np.sqrt(electron_mass * l * l / np.pi**3)
+WB = 1.0e-4 * e * R * R * B * B / (1836.7 * electron_mass)
+DELTA_T = T_MAX - T_MIN
+DELTA_N = N_MAX - N_MIN
+NORMC1 =  1.0e-10 * R * R * np.sqrt(e * DELTA_T) / (h * DELTA_N)
 #--------------------------------
 # MODEL PARAMETERS
 #--------------------------------
@@ -94,150 +94,84 @@ power loss rates:
     p_rad_0: <E_rad^0><sigma nu>_rad^0 n
 ------------------------------"""
 
-sigma_ion = RateCoeff2D('../../data/adas/scd96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
-sigma_rec = RateCoeff2D('../../data/adas/acd96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
-p_rec = RateCoeff2D('../../data/adas/prb96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
-p_rad_i = RateCoeff2D('../../data/adas/plt96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
-p_rad_0 = RateCoeff2D('../../data/adas/prc96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
+sigma_ion = sv('../../data/adas/scd96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
+sigma_rec = sv('../../data/adas/acd96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
+p_rec = sv('../../data/adas/prb96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
+p_rad_i = sv('../../data/adas/plt96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
+p_rad_0 = sv('../../data/adas/prc96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
 
 
 # -------------------------------
 # Functions
 # -------------------------------
 
-def ode_system(X, y):
-    #----------------------------
-    # Preamble
-    #----------------------------
-    rho = X[:, 0:1]
-    rho_eps = torch.clamp(rho, min=1e-12) #no puede ser cero, las ecs explotan
+def ode(rho, y):
     n_hat = y[:, 0:1]
     T_hat = y[:, 1:2]
+    dn_rho = dde.grad.jacobian(y, rho, i=0)
+    dT_rho = dde.grad.jacobian(y, rho, i=1)
+    d2n_rho = dde.grad.hessian(y, rho, component=0)
+    d2T_rho = dde.grad.hessian(y, rho, component=1)
+    nf = lambda x : x + T_MIN / DELTA_T
+    Tf = lambda x : x + T_MIN / DELTA_T
+    nD = nf(n_hat)
+    TD = Tf(T_hat)
+    n = nD * DELTA_N
+    T = TD * DELTA_T
+    NnD = - n * sigma_rec.rate(n, T) + N0 * sigma_ion.rate(n, T)
+    SnD = n * (p_rad_i.rate(n, T) + p_rec.rate(n, T))  + N0 * (p_rad_0.rate(n, T) + E_ION * sigma_ion.rate(n,T))
 
-    n = n_hat * N_MAX
-    T = T_hat * T_MAX
+    Pow = P_EXT / (np.sqrt(2.0 * np.pi) * VAR * DELTA_N * DELTA_T) * torch.exp(-0.5 * (1 / VAR)**2 * (rho - MU)**2)
 
-    dn_rho = dde.grad.jacobian(y, X, i=0, j=0) #es sobre las normalizadas
-    dT_rho = dde.grad.jacobian(y, X, i=1, j=0)
+    term11 = nD * TD * (rho * d2n_rho + dn_rho)
+    term12 = rho * TD * dn_rho * dn_rho
+    term13 = -0.5 * rho * nD * dn_rho * dT_rho
+    term14 = rho * NORMC1 * nD * TD * torch.sqrt(TD) *NnD
+    ode1 = term11 + term12 + term13 + term14
 
-    # T^3/n and log(T^3/n)
-    T3n = (T_hat ** 3) / (n_hat)
-    logT3n = torch.log(T3n)
-    D_hat = n_hat / torch.sqrt(T_hat) * (logT3n + C)
-
-    #----------------------------
-    # ODE: Particles
-    #----------------------------
-    ionization = sigma_ion.rate(n, T) * n_hat**2
-    recombination = sigma_rec.rate(n, T) * n_hat * N_0
-
-
-    S_particle = (R**2 / (D_0 * N_MAX)) * (- ionization + recombination)
-    flux_n = rho_eps * D_hat * dn_rho
-    dflux_n_rho = dde.grad.jacobian(flux_n, X, i=0, j=0)
-
-    ode1 = dflux_n_rho / rho_eps  - S_particle
-
-    #----------------------------
-    # ODE: Energy
-    #----------------------------
-
-    flux_T = rho_eps * D_hat * dT_rho
-    dflux_T_rho = dde.grad.jacobian(flux_T, X, i=0, j=0)
-    conduction = dflux_T_rho / rho_eps
-
-    gaussian_profile = torch.exp(-0.5 * ((rho - MU) / VAR)**2)
-    power_dep = (P_EXT * R / (N_MAX * T_MAX * D_0)) * gaussian_profile
-
-    P_rec = p_rec.rate(n, T)
-    P_rad_i = p_rad_i.rate(n,T)
-    P_rad_0 = p_rad_0.rate(n,T)
-
-    P_loss = P_rec + P_rad_i + P_rad_0
-
-    loss_power = (R**2 / (N_MAX * T_MAX * D_0)) * P_loss
-
-    ode2 = conduction + power_dep - loss_power #me falta agregar coulomb
-
-    #ode1 = torch.nan_to_num(ode1, nan=0.0)
-    #ode2 = torch.nan_to_num(ode2, nan=0.0)
-
+    term21 = 4.7 * nD * nD * TD * (rho * d2T_rho + dT_rho)
+    term22 = 10.4 * rho * nD * TD * dn_rho * dT_rho
+    term23 = -2.35 * rho * nD * nD * dT_rho * dT_rho
+    term24 = 3.0e-4 * WB * nD * nD * TD / DELTA_T
+    term25 = - NORMC1 * rho * TD * torch.sqrt(TD) * TD * NnD
+    term26 = 1.0
+    ode2 = term21 + term22 + term23 + term24 + term25
     return [ode1, ode2]
 
-def boundary_l(X, on_boundary):
-    return on_boundary and dde.utils.isclose(X[0], 0)
+def dn_op(rho, y, X):
+    dn_rho = dde.grad.jacobian(y, rho, i=0, j=0)
+    return dn_rho
 
-def boundary_r(X, on_boundary):
-    return on_boundary and dde.utils.isclose(X[0], 1)
+def dT_op(rho, y, X):
+    dT_rho = dde.grad.jacobian(y, rho, i=1, j=0)
+    return dT_rho
 
-def robin_n(X, y):
-    n_hat = y[:, 0:1]
-    T_hat = y[:, 1:2]
+def boundary(rho, on_boundary):
+    return on_boundary and dde.utils.isclose(rho[0], 0)
 
-    # T^3/n and log(T^3/n)
-    T3n = (T_hat ** 3) / (n_hat + EPS)
-    logT3n = torch.log(T3n)
-    D_hat = n_hat / torch.sqrt(T_hat + EPS) * (logT3n + C)
-    #D_hat = torch.clamp(D_hat, min=1e-8, max=1e8)
+geom = dde.geometry.TimeDomain(0.0, 1.0)
+bc1 = dde.icbc.IC(geom, lambda rho : 1.0, boundary, component=0)
+bc2 = dde.icbc.IC(geom, lambda rho : 1.0, boundary, component=1)
+bc3 = dde.icbc.OperatorBC(geom, dn_op, boundary)
+bc4 = dde.icbc.OperatorBC(geom, dT_op, boundary)
 
-    return -n_hat / (LAMBDA_N * D_hat)
+x_eval = geom.uniform_points(OBSERVATIONS, True)
+solver = pinn(1, HIDDEN_LAYERS, 2, NUM_DOMAIN, NUM_BOUNDARY, NUM_TEST, ADAM_ITER1, ADAM_ITER2)
+sol, res = solver.sol(ode, geom, [bc1, bc2, bc3, bc4], x_eval, weights=LOSS_WEIGHTS, refinement=True)
 
-def robin_T(X, y):
-    n_hat = y[:, 0:1]
-    T_hat = y[:, 1:2]
+#P0 = P_EXT / (np.sqrt(2.0 * np.pi) * VAR * DELTA_N * DELTA_T)
+#print(f"{P0:.3e}")
 
-    # T^3/n and log(T^3/n)
-    T3n = (T_hat ** 3) / (n_hat + EPS)
-    logT3n = torch.log(T3n)
-    D_hat = n_hat / torch.sqrt(T_hat + EPS) * (logT3n + C)
-    #D_hat = torch.clamp(D_hat, min=1e-8, max=1e8)
+plt.scatter(x_eval, sol[:,0], marker='h', color='k', label="n(x) PINN")
+plt.scatter(x_eval, sol[:,1], marker='p', color='b', label="T(x) PINN")
+plt.xlabel("x")
+plt.ylabel("Solution")
+plt.legend()
+plt.show()
+print("success")
 
-    return -T_hat / (LAMBDA_T * D_hat)
+"""
+for i in range(x_eval.shape[0]):
+    print(x_eval[i][0], " ", sol[i, 0], " ", sol[i,1])
+"""
 
-
-#--------------------------------
-# Main
-# -------------------------------
-geom = dde.geometry.Interval(0.0, 1.0)
-
-bc_l_n = dde.icbc.DirichletBC(geom, lambda x: 1.0, boundary_l, component=0)
-bc_l_T = dde.icbc.DirichletBC(geom, lambda x: 1.0, boundary_l, component=1)
-
-bc_r_n = dde.icbc.RobinBC(geom, robin_n, boundary_r, component=0)
-bc_r_T = dde.icbc.RobinBC(geom, robin_T, boundary_r, component=1)
-
-boundaries = [bc_l_n, bc_l_T, bc_r_n, bc_r_T]
-
-data = dde.data.PDE(geom, ode_system, boundaries, NUM_DOMAIN, NUM_BOUNDARY, num_test=NUM_TEST)
-
-net = dde.nn.FNN([1] + HIDDEN_LAYERS*6 + [2], "tanh", "Glorot normal")
-
-def output_transform(x, y):
-    return torch.exp(y)
-
-net.apply_output_transform(output_transform)
-
-net.apply_output_transform(lambda x, y: torch.exp(y))
-
-model = dde.Model(data, net)
-
-model.compile("adam", lr=1e-4, loss_weights=LOSS_WEIGHTS)
-model.train(iterations=ADAM_ITER1)
-
-model.compile("adam", lr=1e-5, loss_weights=LOSS_WEIGHTS)
-model.train(iterations=ADAM_ITER2)
-
-model.compile("L-BFGS")
-model.train()
-
-x = np.linspace(0, 1, OBSERVATIONS)[:, None]
-y = model.predict(x)
-
-n_pred = y[:, 0]
-T_pred = y[:, 1]
-
-with open(pathOutput, 'w') as f:
-    for i in range(len(n_pred)):
-        print(x[i][0], " ", n_pred[i], " ", T_pred[i], file=f, sep='')
-
-print("Output written in '../../data/profile'")
---
+++ src/drivers/pinn_v1.py
@@ -0,0 +1,243 @@
+# -------------------------------
+# Modules
+# -------------------------------
+import numpy as np
+from ruamel.yaml import YAML
+import argparse
+import torch
+import torch.nn.functional as F
+import deepxde as dde
+import matplotlib.pyplot as plt
+from scipy.constants import epsilon_0, e, electron_mass
+from cherab.core.atomic import hydrogen
+from cherab.openadas.parse.adf11 import parse_adf11
+
+import sys
+import os
+
+sys.path.append(os.path.abspath('../modules'))
+
+from sigmav2D import RateCoeff2D
+
+parser = argparse.ArgumentParser(description='Parameter file name')
+
+parser.add_argument('--f', action='store', dest='param_file', type=str,
+                    help='[STR] filename of the parameter file')
+parser.add_argument('--o', action='store', dest='out_file', type=str,
+                    help='[STR] filename of the output file')
+
+
+args = parser.parse_args()
+param_file = args.param_file
+out_file = args.out_file
+assert param_file != None, 'Input File was not find. Make sure it exists in the path "../../parameters". Usage --f [input filename] --o [output filename]'
+assert out_file != None, 'Output file not given. Usage --f [input filename] --o [output filename]'
+
+yaml = YAML(typ='safe')
+pathInput = '../../parameters/' + param_file
+pathOutput = '../../data/profile/' + out_file
+
+with open(pathInput, 'r') as file:
+    params = yaml.load(file)
+# -------------------------------
+# Global Constants and Parameters
+# -------------------------------
+B = params['physical_params']['B'] #T
+E_ION = params['physical_params']['E_ION'] #eV
+N_0 = params['physical_params']['N_0'] # cm^{-3}
+N_MIN = params['physical_params']['N_MIN']
+T_MIN = params['physical_params']['T_MIN']
+N_MAX = params['physical_params']['N_MAX'] # eV
+T_MAX = params['physical_params']['T_MAX'] # cm^-3
+R = params['physical_params']['R'] # cm
+LAMBDA_N = params['physical_params']['LAMBDA_N']
+LAMBDA_T = params['physical_params']['LAMBDA_T']
+P_EXT = params['physical_params']['P_EXT'] # cm^{-3}
+VAR = params['physical_params']['VAR']
+MU = params['physical_params']['MU']
+
+D_0 = 1e4 * (2 * np.sqrt(2 * np.pi * electron_mass) / 3) * (e / (4 * np.pi * epsilon_0 * B))**2 * (N_MAX * 1e6  / np.sqrt(T_MAX * e)) #cm^2 s^-1
+KAPPA_0 = 4.7 * N_MAX * D_0 # cm^-1 s^-1
+C = 2 * np.log(4 * np.pi * np.power(epsilon_0 * T_MAX * e, 1.5) / (e**3 * np.sqrt(N_MAX)))
+
+#--------------------------------
+# MODEL PARAMETERS
+#--------------------------------
+HIDDEN_LAYERS = params['hidden_layers']
+NUM_DOMAIN = params['model_params']['NUM_DOMAIN']
+NUM_BOUNDARY = params['model_params']['NUM_BOUNDARY']
+NUM_TEST = params['model_params']['NUM_TEST']
+LOSS_WEIGHTS = params['model_params']['LOSS_WEIGHTS']
+ADAM_ITER1 = params['model_params']['ADAM_ITER1']
+ADAM_ITER2 = params['model_params']['ADAM_ITER2']
+OBSERVATIONS = params['num_observations']
+
+# -------------------------------
+# Device Settings
+# -------------------------------
+DTYPE = torch.float64
+EPS   = params['epsilon']
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+torch.set_default_device(DEVICE)
+
+
+"""------------------------------
+Rate objects
+
+Particle sources and drains:
+    sigma_ion: <sigma nu>_ion n0 * n
+    sigma_rec: <sigma nu>_rec n^2
+
+power loss rates:
+    p_rec = <E_rec><sigma nu>_rec n ^ 2 (contains Bremsstrahlung radiation)
+    p_rad_i: <E_rad^i><sigma nu>_rad^i n^2
+    p_rad_0: <E_rad^0><sigma nu>_rad^0 n
+------------------------------"""
+
+sigma_ion = RateCoeff2D('../../data/adas/scd96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
+sigma_rec = RateCoeff2D('../../data/adas/acd96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
+p_rec = RateCoeff2D('../../data/adas/prb96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
+p_rad_i = RateCoeff2D('../../data/adas/plt96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
+p_rad_0 = RateCoeff2D('../../data/adas/prc96_h.dat', hydrogen, n_max=N_MAX, T_max = T_MAX)
+
+
+# -------------------------------
+# Functions
+# -------------------------------
+
+def ode_system(X, y):
+    #----------------------------
+    # Preamble
+    #----------------------------
+    rho = X[:, 0:1]
+    rho_eps = torch.clamp(rho, min=1e-12) #no puede ser cero, las ecs explotan
+    n_hat = y[:, 0:1]
+    T_hat = y[:, 1:2]
+
+    n = n_hat * N_MAX
+    T = T_hat * T_MAX
+
+    dn_rho = dde.grad.jacobian(y, X, i=0, j=0) #es sobre las normalizadas
+    dT_rho = dde.grad.jacobian(y, X, i=1, j=0)
+
+    # T^3/n and log(T^3/n)
+    T3n = (T_hat ** 3) / (n_hat)
+    logT3n = torch.log(T3n)
+    D_hat = n_hat / torch.sqrt(T_hat) * (logT3n + C)
+
+    #----------------------------
+    # ODE: Particles
+    #----------------------------
+    ionization = sigma_ion.rate(n, T) * n_hat**2
+    recombination = sigma_rec.rate(n, T) * n_hat * N_0
+
+
+    S_particle = (R**2 / (D_0 * N_MAX)) * (- ionization + recombination)
+    flux_n = rho_eps * D_hat * dn_rho
+    dflux_n_rho = dde.grad.jacobian(flux_n, X, i=0, j=0)
+
+    ode1 = dflux_n_rho / rho_eps  - S_particle
+
+    #----------------------------
+    # ODE: Energy
+    #----------------------------
+
+    flux_T = rho_eps * D_hat * dT_rho
+    dflux_T_rho = dde.grad.jacobian(flux_T, X, i=0, j=0)
+    conduction = dflux_T_rho / rho_eps
+
+    gaussian_profile = torch.exp(-0.5 * ((rho - MU) / VAR)**2)
+    power_dep = (P_EXT * R / (N_MAX * T_MAX * D_0)) * gaussian_profile
+
+    P_rec = p_rec.rate(n, T)
+    P_rad_i = p_rad_i.rate(n,T)
+    P_rad_0 = p_rad_0.rate(n,T)
+
+    P_loss = P_rec + P_rad_i + P_rad_0
+
+    loss_power = (R**2 / (N_MAX * T_MAX * D_0)) * P_loss
+
+    ode2 = conduction + power_dep - loss_power #me falta agregar coulomb
+
+    #ode1 = torch.nan_to_num(ode1, nan=0.0)
+    #ode2 = torch.nan_to_num(ode2, nan=0.0)
+
+    return [ode1, ode2]
+
+def boundary_l(X, on_boundary):
+    return on_boundary and dde.utils.isclose(X[0], 0)
+
+def boundary_r(X, on_boundary):
+    return on_boundary and dde.utils.isclose(X[0], 1)
+
+def robin_n(X, y):
+    n_hat = y[:, 0:1]
+    T_hat = y[:, 1:2]
+
+    # T^3/n and log(T^3/n)
+    T3n = (T_hat ** 3) / (n_hat + EPS)
+    logT3n = torch.log(T3n)
+    D_hat = n_hat / torch.sqrt(T_hat + EPS) * (logT3n + C)
+    #D_hat = torch.clamp(D_hat, min=1e-8, max=1e8)
+
+    return -n_hat / (LAMBDA_N * D_hat)
+
+def robin_T(X, y):
+    n_hat = y[:, 0:1]
+    T_hat = y[:, 1:2]
+
+    # T^3/n and log(T^3/n)
+    T3n = (T_hat ** 3) / (n_hat + EPS)
+    logT3n = torch.log(T3n)
+    D_hat = n_hat / torch.sqrt(T_hat + EPS) * (logT3n + C)
+    #D_hat = torch.clamp(D_hat, min=1e-8, max=1e8)
+
+    return -T_hat / (LAMBDA_T * D_hat)
+
+
+#--------------------------------
+# Main
+# -------------------------------
+geom = dde.geometry.Interval(0.0, 1.0)
+
+bc_l_n = dde.icbc.DirichletBC(geom, lambda x: 1.0, boundary_l, component=0)
+bc_l_T = dde.icbc.DirichletBC(geom, lambda x: 1.0, boundary_l, component=1)
+
+bc_r_n = dde.icbc.RobinBC(geom, robin_n, boundary_r, component=0)
+bc_r_T = dde.icbc.RobinBC(geom, robin_T, boundary_r, component=1)
+
+boundaries = [bc_l_n, bc_l_T, bc_r_n, bc_r_T]
+
+data = dde.data.PDE(geom, ode_system, boundaries, NUM_DOMAIN, NUM_BOUNDARY, num_test=NUM_TEST)
+
+net = dde.nn.FNN([1] + HIDDEN_LAYERS*6 + [2], "tanh", "Glorot normal")
+
+def output_transform(x, y):
+    return torch.exp(y)
+
+net.apply_output_transform(output_transform)
+
+net.apply_output_transform(lambda x, y: torch.exp(y))
+
+model = dde.Model(data, net)
+
+model.compile("adam", lr=1e-4, loss_weights=LOSS_WEIGHTS)
+model.train(iterations=ADAM_ITER1)
+
+model.compile("adam", lr=1e-5, loss_weights=LOSS_WEIGHTS)
+model.train(iterations=ADAM_ITER2)
+
+model.compile("L-BFGS")
+model.train()
+
+x = np.linspace(0, 1, OBSERVATIONS)[:, None]
+y = model.predict(x)
+
+n_pred = y[:, 0]
+T_pred = y[:, 1]
+
+with open(pathOutput, 'w') as f:
+    for i in range(len(n_pred)):
+        print(x[i][0], " ", n_pred[i], " ", T_pred[i], file=f, sep='')
+
+print("Output written in '../../data/profile'")
